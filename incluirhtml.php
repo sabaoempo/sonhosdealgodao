0f3362f527209c8f0449215f52e153c17bfdc353
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-04-15 18:39:51 -0300

    include header
    header and navbar without loading them everytime you access the pages

---
+++ incluirhtml.php
@@ -0,0 +1,75 @@
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+          
+           
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+          <!--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
+          <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
+          <link rel="stylesheet" type="text/css" href="index.css" />
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+       
+        <header id="header">
+            
+        <img src="imagens/header3.png" alt="Pascoa" width="100%" height="250" style="background-image: url(imagens/nav.png);">
+        
+  <!--      <nav class="navbar navbar-expand-lg " style="background-color: rgb(197, 106, 106)">-->
+
+  <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">-->
+  <!--  <span class="navbar-toggler-icon"></span>-->
+  <!--</button>-->
+  <!--<div class="collapse navbar-collapse" id="navbarNavDropdown">-->
+  <!--  <ul class="navbar-nav">-->
+  <!--    <li class="nav-item active">-->
+  <!--      <a class="nav-link" id="color:hover"href="index.html" type="bottom"  >Home </a>-->
+  <!--    </li>-->
+  <!--    <li class="nav-item">-->
+  <!--      <a class="nav-link" href="#">Sobre N贸s</a>-->
+  <!--    </li>-->
+     
+  <!--    <li class="nav-item">-->
+  <!--    <a href="login.php" class="nav-link">Entre</a></li>-->
+  <!--    <li class="nav-item">-->
+  <!--    <a href="register.php" class="nav-link">Cadastre-se</a></li>-->
+  <!--    <li class="nav-item dropdown" >-->
+  <!--        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pedidos</a>-->
+  <!--        <div class="dropdown-menu" style="z-index: 2;">-->
+  <!--          <a class="dropdown-item" href="sonhosdealgodao.tk/CadastroProdutoTeste.php">Bombom</a>-->
+  <!--          <a class="dropdown-item" href="#">Ovo</a>-->
+  <!--        </div>-->
+  <!--      </li>-->
+     
+     
+  <!--  </nav>-->
+       
+       <ul class="nav nav-pills" style="background-color: rgb(197, 106, 106)">
+          <li class="nav-item">
+            <a class="nav-link " href="index.php" style="color:aliceblue; font-size: 22px">Home</a>
+          </li>
+         
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="color:aliceblue; font-size: 22px">Sobre n&#0243s</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="color:aliceblue; font-size: 22px" href="login.php">Entre</a>
+          </li>
+          <li class="nav-item">
+              <a class="nav-link" style="color:aliceblue; font-size: 22px" href="register.php">Cadastre-se</a>
+            </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" style="color:aliceblue; font-size: 22px" data-toggle="dropdown" href="#">Pedidos</a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="CadastroProdutoTeste.php">Bombom</a>
+               
+                <a class="dropdown-item" href="#">Ovo</a></div>
+            </li>
+        </ul>
+    </header>
+    </body>
+</html>
