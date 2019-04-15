f0c20cebb536fc65e3ca4c5d380db6de050491cf
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-04-15 18:38:04 -0300

    update index without header
    looks ok for now (see "pedidos")

--- index.php
+++ index.php
@@ -1,69 +1,22 @@
+ <?php include('functions.php'); ?>
+ <?php include('incluirhtml.php'); ?>
 
     <html>
-        <head>
-          
+        <head><meta http-equiv="Content-Type" content="text/html; charset=shift_jis">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-           
           <meta name="viewport" content="width=device-width, initial-scale=1">
-    
           <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+          <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>-->
+          
           <link rel="stylesheet" type="text/css" href="index.css" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-           <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-
-<title>Sonhos de Algodão</title>
-
-    </head>
-    <header>
-       
-        <img src="imagens/header3.png" alt="Pascoa" width="100%" height="250" style="background-image: url(imagens/nav.png);">
-       
-    </header>
+          <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">-->
+          <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
+          <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>-->
+          <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>-->
+            <title>Sonhos de Algodﾃ｣o</title>
+        </head>
     <!-- <script>$('.dropdown-toggle').dropdown()</script> -->
     <body>
-   
-
-    
-    
-    
-    
- 
-        <nav class="navbar navbar-expand-lg " style="background-color: rgb(197, 106, 106)">
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" id="color:hover"href="index.html" type="bottom"  >Home </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Sobre Nós</a>
-      </li>
-     
-      <li class="nav-item">
-      <a href="login.php" class="nav-link">Entre</a></li>
-      <li class="nav-item">
-      <a href="register.php" class="nav-link">Cadastre-se</a></li>
-      <li class="nav-item dropdown" >
-          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Pedidos</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Bombom</a>
-            <a class="dropdown-item" href="#">Ovo</a>
-
-          </div>
-        </li>
-     
-     
-    </nav>
-     
-        
-            
-         
 
         <div id="demo" class="carousel slide" data-ride="carousel">
             <ul class="carousel-indicators">
@@ -73,16 +26,16 @@
             </ul>
             <div class="carousel-inner">
               <div class="carousel-item active">
-                <img src="imagens/Páscoa.png" alt="Pascoa" width="1700" height="500">
+                <img src="imagens/Paﾌ《coa.png" alt="Pascoa" width="1700" height="500">
                 <div class="carousel-caption"  style="right: -21%">
-                  <h3 style="font-size: 30px;">Páscoa</h3>
+                  <h3 style="font-size: 30px;">Pﾃ｡scoa</h3>
                   <p style="font-size: 45px;">Venha personalizar seu produto</p>
                 </div>   
               </div>
               <div class="carousel-item">
                 <img src="imagens/ovos.jpg" alt="Ovo" width="1700" height="500">
                 <div class="carousel-caption" style="right: -50%; color: #B22222">
-                    <h3 style="font-size: 30px;">Páscoa</h3>
+                    <h3 style="font-size: 30px;">Pﾃ｡scoa</h3>
                     <p style="font-size: 30px;">Ovos de colher</p>
                 </div>   
               </div>
