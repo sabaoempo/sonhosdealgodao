3a4df0bb3fb56e2b25631693f2f77218509d807b
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-04-16 15:46:42 -0300

    admin page
    with vendor information from database

--- admin.php
+++ admin.php
@@ -1,6 +1,18 @@
 <html>
     <?php
         include('functions.php');
+        $servername = 'localhost';
+        $nome_db    = "sonhosdealgodao_sonhos";
+        $user_db    = "sonhosdealgodao_dev";
+        $pass_db    = "$0nh0_d3_algod4o";
+        $db;
+
+        $db = mysqli_connect($servername, $user_db, $pass_db, $nome_db) or die(mysqli_error());
+        mysqli_select_db($db, $nome_db);
+        $query_vendor = "SELECT * FROM users WHERE ID != 4";
+        $result = mysqli_query($db, $query_vendor);
+        $vendorData = mysqli_fetch_array($result);
+        
     ?>
     <head>
         
@@ -15,15 +27,25 @@
             include('incluirhtml.php');
             $user = $_SESSION['user'];
         }
-        // if($_SESSION['user']){
-            
-        // }else{
-        //     header("location:index.php");
-        // }
         
     ?>
     <body>
         <h3>Seja bem vindo, <?php echo $user?></h3><br>
         <a href="logout.php">Sair</a><br><br>
+        <a href="ManipulaVendas.php">Administrar vendas</a>
+        <center>
+            <table>
+                <tr>
+                    <th>
+                        Vendedores
+                    </th>
+                </tr>
+                <?php
+                    do{
+                        echo "<tr><td><a href='vendorData.php?id=".$vendorData[ID]."' target='_blank' class='$vendorData[ID]'>$vendorData[USERNAME]</a></td></tr>";
+                    }while($vendorData = mysqli_fetch_array($result));
+                ?>
+            </table>
+        </center>
     </body>
 </html>
