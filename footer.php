ca8b21d8a9e88a996bf9cf2d2e63a7a05391faa0
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-05-29 13:56:10 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,26 @@
+ <link rel="stylesheet" type="text/css" href="index.css" />
+ <section id="footer">
+        <div class="container">
+          <div class="row text-center text-xs-center text-sm-left text-md-left">
+            
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+              <ul class="list-unstyled list-inline social text-center">
+                <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-facebook"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void();"><i class="fa fa-google-plus"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void();" target="_blank"><i class="fa fa-envelope"></i></a></li>
+              </ul>
+            </div>
+            </hr>
+          </div>	
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+              <p><u><a href="https://www.nationaltransaction.com/"></a></u> </p>
+              <p class="h6">&copy Todos os direitos reservados.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank"></a></p>
+            </div>
+            </hr>
+          </div>	
+        </div>
+      </section>
