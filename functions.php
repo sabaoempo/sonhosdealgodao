d0459096d6bc19257a07721d2cbef8d7d05d891d
Ariel Barcelos <ariel.barcelos@sga.pucminas.br>
2019-05-29 14:02:20 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,6 @@
 <?php
-session_start();
-$servername = 'localhost';
-$nome_db    = "sonhosdealgodao_sonhos";
-$user_db    = "sonhosdealgodao_dev";
-$pass_db    = "$0nh0_d3_algod4o";
-$db;
+$db = mysqli_connect('localhost', 'sonhosdealgodao_dev', '$0nh0_d3_algod4o', 'sonhosdealgodao_sonhos') or die(mysqli_error());
 
-$db = mysqli_connect($servername, $user_db, $pass_db, $nome_db) or die(mysqli_error());
-
-$username = "";
-$email    = "";
-$errors   = array();
 
 if (isset($_POST['register_btn'])) {
     register();
@@ -22,35 +12,25 @@ if (isset($_POST['login'])) {
 
 function register()
 {
-    global $db, $errors, $username, $email;
-    
+    global $db;
     $username   = $_POST['username'];
     $email      = $_POST['email'];
     $password_1 = $_POST['password_1'];
     $password_2 = $_POST['password_2'];
-    
-    
-    if (empty($username)) {
-        array_push($errors, "Por favor, insira um nome de usuário.");
-    }
-    if (empty($email)) {
-        array_push($errors, "Por favor, digite um endereço de e-mail.");
-    }
-    if (empty($password_1)) {
-        array_push($errors, "Por favor, insira uma senha.");
-    }
+
     if ($password_1 != $password_2) {
-        array_push($errors, "As duas senhas não coincidem.");
         Print '<script>alert("As senhas não coincidem.");</script>';
     }
     
-    if (count($errors) == 0) {
-        $password = md5($password_1);
-            $query = "INSERT INTO users (USERNAME, EMAIL, USERTYPE, PASSWORD) VALUES ('$username','$email','user','$password')";
+    else {
+            $password = md5($password_1);
+            $query = "INSERT INTO users (USERNAME, EMAIL, USERTYPE, PASSWORD, IMAGE) VALUES ('$username','$email','user','$password', 'perfil/anon.svg')";
             mysqli_query($db, $query);
-            $logged_in_user_id   = mysqli_insert_id($db);
-            $_SESSION['user']    = getUserById($logged_in_user_id);
-            $_SESSION['success'] = "Você está cadastrado";
+            $queryUser = 'SELECT * FROM users WHERE password_1 = '.$password;
+            $result = mysqli_query($db, $queryUser);
+            $userData = mysqli_fetch_assoc($result);
+            $_SESSION['user'] = $userData['username'];
+            // echo $_SESSION['user'];
             Print '<script>alert("Registro feito com sucesso");</script>';
             
         
@@ -67,7 +47,7 @@ function getUserById($id)
 }
 
 function isUser($username){
-    global $db;
+    $db = mysqli_connect('localhost', 'sonhosdealgodao_dev', '$0nh0_d3_algod4o', 'sonhosdealgodao_sonhos') or die(mysqli_error());
     $query = "SELECT * FROM users WHERE USERNAME='$username' AND USERTYPE='user'";
     $result = mysqli_query($db, $query);
     $exists = mysqli_num_rows($result);
@@ -75,7 +55,7 @@ function isUser($username){
 }
 
 function isAdmin($username){
-    global $db;
+    $db = mysqli_connect('localhost', 'sonhosdealgodao_dev', '$0nh0_d3_algod4o', 'sonhosdealgodao_sonhos') or die(mysqli_error());
     $query = "SELECT * FROM users WHERE USERNAME='$username' AND USERTYPE='admin'";
     $result = mysqli_query($db, $query);
     $exists = mysqli_num_rows($result);
@@ -101,10 +81,10 @@ function display_error()
 
 function login()
 {
-    global $db, $nome_db;
-    
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
+    global $nome_db;
+    $db = mysqli_connect('localhost', 'sonhosdealgodao_dev', '$0nh0_d3_algod4o', 'sonhosdealgodao_sonhos') or die(mysqli_error());
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     $query    = "SELECT USERNAME FROM users WHERE USERNAME = '$username' AND PASSWORD =md5('$password')";
     mysqli_select_db($nome_db, $db);
     $result   = mysqli_query($db, $query);
@@ -117,6 +97,8 @@ function login()
         $_SESSION['user'] = $username;
         Print '<script>window.location="admin.php";</script>';
         exit();
+    } else {
+        Print '<script>alert("Você não tem acesso a essa página");</script>';
     }
     
     
@@ -124,11 +106,12 @@ function login()
 }
 
 function isLoggedIn(){
-    if(isset($_SESSION['user'])){
-        return true;
-    } else {
-        return false;
-    }
+    // if(isset($_SESSION['user'])){
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return isset($_SESSION['user'])? true : false;
 }
 
 mysqli_close($db);
